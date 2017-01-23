9bdd7dc938ef669b449b8e8f638b12e1e70a3594
Katharine Tallaksen <tallaksen@gatech.edu>
2017-01-23 10:52:32 -0500

    added division into frames, histogram, and computed entropy

--- segmentation.py
+++ segmentation.py
@@ -6,9 +6,9 @@ Steps:
 1. Pre-processing stage (add a pre-emph filter + other filter)
 2. Choose speech window
 3. Determine short time prob. distr. using histogram
-4. Threshold the entropy profile
+4. Compute weighted entropy of speech frame
 5. Determine the adaptive threshold for entropy profile
-6. Compute weighted entropy of speech frame
+6. Threshold the entropy profile
 7. Apply speech segment size constraint
 8. Appy intra-segment distance constraint
 9. Yay!
@@ -20,16 +20,14 @@ import numpy as np
 import scipy
 from scipy.fftpack import rfft, irfft, fftfreq
 from scipy.io import wavfile
-from scipy.signal import firwin, remez, kaiser_atten, kaiser_beta
 import matplotlib.pyplot as plt
 import python_speech_features as speech
 
-samples = ['samples/sf1_fi1']
+sample = 'samples/sf1_fi1' # To be added feature: make this an argument passed in by the user
 
-for sample in samples:
-	input_filename = sample + '.wav'
+input_filename = sample + '.wav'
 
-	rate, data = wavfile.read(input_filename)
+rate, data = wavfile.read(input_filename)
 
 #################################################
 # 			Step 1: Filters
@@ -37,15 +35,84 @@ for sample in samples:
 
 # Pre-emphasis:
 
-	preemp_data = speech.sigproc.preemphasis(data, 0.97)
+preemp_data = speech.sigproc.preemphasis(data, 0.97)
+signal = preemp_data
 
-#	fig = plt.plot(data, 'm', filtered_data, 'b')
-#	plt.setp(fig, linewidth=0.25)
-#	plt.savefig('foo.png')
+################# XM STUFF GOES HERE #######
+# Select frequency components 250 Hz < f < 6000 Hz
 
-# Add a band-pass filter to remove constant and low freq background components,
-# and high freq noise and speech harmonics
+#################################################
+# 			Step 2: Divide into windows
+#################################################
 
+# Divide the speech into overlapping frames with 25-50% overlap
+
+# Find duration of sample so we can divide it into 20 ms segments (because inertia of glottis?)
+
+num_samples = len(signal)
+
+duration = num_samples / float(rate)
+
+# Need to define the length of each frame (in samples) and the number of samples after the start of the prev. 
+# frame that the next frame should begin
+
+frame_duration_in_seconds = 0.02
+
+num_windows = int(duration / frame_duration_in_seconds)
+
+frame_length = num_samples / float(num_windows)
+
+amount_overlap = 0.5 # 25-50%
+
+frame_step = frame_length * amount_overlap
+
+# plug into python speech features function for dividing into overlapping frames
+
+frames = speech.sigproc.framesig(signal, frame_length, frame_step)
+
+#################################################
+# Step 3: Determine short time prob. distr.
+#################################################
+
+# Take the FFT of each frame, then use histogram to find prob. distribution
+
+# Construct a histogram with N bins for each frame
+# N will be 50-100, choice depending on sensitivity and computational load
+
+# iterate through the frames and create a histogram for each
+
+entropy_profile = np.empty([len(frames)])
+
+for i in range(len(frames)):
+	#print i
+
+	# Take FFT
+	frame_freq = scipy.fftpack.fft(frames[i])
+	#print frame_freq
+
+	# number of bins
+	N = 50
+
+	# take histogram
+
+	frame_hist, frame_bin_edges = np.histogram(frame_freq, N, None, True, None, None )
+
+
+#################################################
+# Step 4: Compute the weighted entropy per frame
+#################################################
+
+	# Calculate the entropy using 
+
+	# H(X) = -Sum_(k=1)^N p_k log(p_k)
+
+	H = scipy.stats.entropy(frame_hist)
+	#print H
+
+	entropy_profile[i] = H
+
+
+'''
 # Use fft to recover frequency from our filtered amplitude
 
 	#k = arange(len(filtered_data))
@@ -70,12 +137,4 @@ for sample in samples:
 
 	# after filtering, recover our signal
 
-	recovered_signal = scipy.ifft(shifted)
-
-#################################################
-# 			Step 1: Divide into windows
-#################################################
-
-	# Divide the speech into overlapping frames with 25-50% overlap
-
-	frames = speech.sigproc.framesig(recovered_signal, )
+	recovered_signal = scipy.ifft(shifted)'''
